e7817a4fb9b4bc860dd9d68746ccf36064aef198
Gamalliel Sharon <you@example.com>
2021-02-23 16:01:23 +0700

    first day of 100 days

---
+++ Day1/main.py
@@ -0,0 +1,64 @@
+# Write your code below this line 👇
+# Day 1
+
+#==== 1 ====
+print('===== 1. First Program =====')
+print("hello world")
+
+#===== 2 =====
+print('===== 2. Print a string =====')
+print('Day 1 - Python print function')
+print('The function is declared like this:')
+print("print('what to print')")
+
+#===== 3 =====
+print('===== 3. New Lines =====')
+print('Hello World\nHello World')
+
+#===== 4 =====
+print('===== 4. String Concatenation =====')
+print('Hello' + ' ' + 'Gamal')
+
+#===== 5 =====
+print('===== 5. Fixing Broken Code =====')
+#challenge for debugging
+print("Day 1 - String Manipulation")
+print('String Concatenation is done with the "+" sign.')
+print('e.g. print("Hello " + "world")')
+print("New lines can be created with a backslash and n.")
+
+#===== 6 =====
+print('===== 6. Play with input function =====')
+print('Hello ' + input('whats your name?'))
+
+#Challenge for input function
+a = input("What is your name? ")
+
+print(len(a))
+
+#===== 7 =====
+name = input('What is your name?')
+length = len(name)
+print(length)
+
+#===== 8 =====
+#Variable Challenge
+# 🚨 Don't change the code below 👇
+a = input("a: ")
+b = input("b: ")
+# 🚨 Don't change the code above 👆
+
+####################################
+#Write your code below this line 👇
+
+c = a
+a = b
+b = c
+
+
+#Write your code above this line 👆
+####################################
+
+# 🚨 Don't change the code below 👇
+print("a: " + a)
+print("b: " + b)

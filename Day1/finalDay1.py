a49686331c46de2b3ee1a295e474fb603788529e
Gamalliel Sharon <you@example.com>
2021-02-23 16:17:55 +0700

    adding final project for day1

---
+++ Day1/finalDay1.py
@@ -0,0 +1,10 @@
+#final projects for day 1
+#1. Create a greeting for your program.
+print('Hello! Welcome to the find my band name games!')
+#2. Ask the user for the city that they grew up in.
+a = input('Where are you grew up in?\n')
+#3. Ask the user for the name of a pet.
+b = input('What is your pet name?\n')
+#4. Combine the name of their city and pet and show them their band name.
+print('Your band name could be ' + a + " " + b)
+#5. Make sure the input cursor shows on a new line, see the example at:
